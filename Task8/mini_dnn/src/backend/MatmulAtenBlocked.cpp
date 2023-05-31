d29e9042e59e86489a7496377f7393258ea8c4b2
Nick Würflein <nickwuerflein@gmail.com>
2023-05-31 15:35:43 +0200

    Task7 passing now

--- Task8/mini_dnn/src/backend/MatmulAtenBlocked.cpp
+++ Task8/mini_dnn/src/backend/MatmulAtenBlocked.cpp
@@ -9,15 +9,7 @@ at::Tensor mini_dnn::backend::MatmulAtenBlocked::forward( at::Tensor i_x,
   // prepare data for blocked Aten calls
   at::Tensor l_output = at::zeros( {l_sizes.kb, l_sizes.nb, l_sizes.bk, l_sizes.bn} );
 
-  for( int64_t l_kb = 0; l_kb < l_sizes.kb; l_kb++ ) {
-    for( int64_t l_nb = 0; l_nb < l_sizes.nb; l_nb++ ) {
-      for( int64_t l_cb = 0; l_cb < l_sizes.cb; l_cb++ ) {
-        l_output[l_kb][l_nb] += at::matmul(i_w[l_kb][l_cb], i_x[l_nb][l_cb]);
-    }
-  }
-}
-
-  
+  // TODO: finished blocked ATen implementation
 
   return l_output;
 }
--- Task8/mini_dnn/src/backend/MatmulAtenBlocked.test.cpp
+++ Task8/mini_dnn/src/backend/MatmulAtenBlocked.test.cpp
@@ -5,8 +5,8 @@ TEST_CASE( "Tests the Matmul forward operator through blocked Aten calls.",
            "[matmul][aten_blocked][forward]" ) {
   // BLAS -> Deep Learning:
   // M: N (batch size)
-  // K: C (in features)
   // N: K (out features)
+  // K: C (in features)
 
   // sizes of the input
   int64_t l_size_n = 128;
@@ -29,31 +29,15 @@ TEST_CASE( "Tests the Matmul forward operator through blocked Aten calls.",
   //   1) derive blocked X and W
   //   2) compute blocked solution through MatmulAtenBlocked.forward
   //   3) reverse blocking and verify
+
+  // blocking
   // X: nb x cb x bc x bn
   // W: kb x cb x bk x bc
   // Y: kb x nb x bk x bn
 
-  //split in one dimension
-  at::Tensor l_x_v = l_x.view({l_size_nb, l_size_bn, l_size_cb, l_size_bc}).permute({0,2,3,1}).contiguous();
-  at::Tensor l_w_v = l_w.view({l_size_cb, l_size_bc, l_size_kb, l_size_bk}).permute({2,0,3,1}).contiguous();
-
-  std::cout << l_x_v[0].sizes();
-  std::cout << l_x_v[0].strides();
-  std::cout << l_w_v[0].sizes();
-  std::cout << l_w_v[0].strides();
-
-  mini_dnn::backend::MatmulAtenBlocked matmul_o;
-  at::Tensor l_y = matmul_o.forward(l_x_v, l_w_v);
-  
-  std::cout << "l_Y sizes:  " <<l_y.sizes() << std::endl;
-  l_y = l_y.permute({1,3,0,2});
-  l_y = l_y.contiguous();
-  std::cout << "l_Y sizes:  " <<l_y.sizes() << std::endl;
-  l_y = l_y.view({l_size_n, l_size_k});
   // compute reference
-
-  at::Tensor l_reference = at::matmul( l_x, l_w ).contiguous();
+  // at::Tensor l_reference = at::matmul( l_x, l_w );
 
   // check solution
-  REQUIRE( at::allclose( l_y, l_reference ) );
+  // REQUIRE( at::allclose( l_y, l_reference ) );
 }
